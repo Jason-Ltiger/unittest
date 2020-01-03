687bb573a83e4f65823b914c3ef30a1b167a4b9b
linsen <351990237@qq.com>
2020-01-03 09:50:23 +0800

    【新特性】   gtest语法特性 【修改类型】 新特性 【修改内容】 1.参数化2.常用测试宏3.testsuit等名词 【外部接口】 涉及外部接口变更？ N 【问题单号】 NULL

--- src/unittest.cpp
+++ src/unittest.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 #include<gtest/gtest.h>
 
+
+#if 0
 struct LinkNode
 {
     int _data;
@@ -145,6 +147,84 @@ TEST_F(TestLink,Delete)
     ASSERT_TRUE(link.Delete(3) == true);
     ASSERT_TRUE(link.Delete(9) == false);
 }
+#endif
+
+//  断言的使用
+#if 0
+int Add(int a, int b)
+{
+    return a + b;
+}
+bool MutuallyPrime(int m, int n)
+{
+    return Add(m, n) < 1;
+}
+
+TEST(PredicateAssertionTest, Demo)
+{
+    int m = 5, n = 6;
+    EXPECT_PRED2(MutuallyPrime, m, n);
+}
+testing::AssertionResult AssertFoo(const char* m_expr, const char* n_expr, const char* k_expr, int m, int n, int k) {
+    if (Add(m, n) == k)
+        return testing::AssertionSuccess();
+    testing::Message msg;
+    msg << m_expr << " 和 " << n_expr << " 的最大公约数应该是：" << Add(m, n) << " 而不是：" << k_expr;
+    return testing::AssertionFailure(msg);
+}
+
+TEST(AssertFooTest, HandleFail)
+{
+    EXPECT_PRED_FORMAT3(AssertFoo, 3, 6, 2);
+}
+
+#endif
+
+
+#if 0
+template <typename T> class FooType {
+public:
+    void Bar() {
+        testing::StaticAssertTypeEq<bool, T>();
+        }
+};
+
+TEST(TypeAssertionTest, Demo)
+{
+    FooType<int> fooType; 
+    fooType.Bar();
+}
+
+
+bool IsPrime(int n)
+{
+    if (n <= 1) return false;
+    if (n % 2 == 0) return n == 2;
+    for (int i = 3; ; i += 2) {
+        if (i > n / i) break;
+
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+class IsPrimeParamTest : public::testing::TestWithParam<int>
+{
+
+};
+
+TEST_P(IsPrimeParamTest, HandleTrueReturn)
+{
+    int n = GetParam();
+    EXPECT_TRUE(IsPrime(n));
+}
+
+INSTANTIATE_TEST_CASE_P(TrueReturn, IsPrimeParamTest, testing::Values(3, 5, 11, 23, 17));
+
+typedef testing::Types<char, int, unsigned int> MyTypes;
+
+#endif
+
 
 int main(int argc,char *argv[])
 {
